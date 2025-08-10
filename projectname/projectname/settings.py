57b09896766081756f0d4ccceb980eb7cf608fad
prokartcopr <prokart.copr@gmail.com>
2025-08-10 14:36:35 +0530

    changes

--- projectname/projectname/settings.py
+++ projectname/projectname/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-nrx_*^qjujdujthz7^%f+0lvuvg5+c6k^e@w-=eq7a2n@x4j7$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['prasanthm.pythonanywhere.com']
+ALLOWED_HOSTS = ['prasanthm.pythonanywhere.com', '127.0.0.1', 'localhost']
+
 
 
 
